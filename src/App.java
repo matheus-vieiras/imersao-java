d6933cec79160e7579f06f1029e4c21820362f52
Matheus Vieira <matheusvieira.contato@hotmail.com>
2022-07-19 19:48:50 -0300

    Gerando figurinhas para WhatsApp

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -12,7 +14,7 @@ public class App {
 
 		// fazer uma conexão HTTP e buscar os top 250 filmes
 		// String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
-		String url = "https://api.mocki.io/v2/549a5d8b/Top250TVs";
+		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
 		URI endereco = URI.create(url);
 		var client = HttpClient.newHttpClient();
 		var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -24,10 +26,18 @@ public class App {
 		List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
 		// exibir e manipular os dados
+		var geradora = new GeradoraDeFigurinhas();
 		for (Map<String, String> filme : listaDeFilmes) {
-			System.out.println(filme.get("title"));
-			System.out.println(filme.get("image"));
-			System.out.println(filme.get("imDbRating"));
+
+			String urlImagem = filme.get("image");
+			String titulo = filme.get("title");
+
+			InputStream inputStream = new URL(urlImagem).openStream();
+			String nomeArquivo = titulo + ".png";
+
+			geradora.cria(inputStream, nomeArquivo);
+
+			System.out.println(titulo);
 			System.out.println();
 		}
 	}
